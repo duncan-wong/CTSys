ea0596ee975de53c9f928be75417fb46292ec430
duncan-wong <duncan1992@gmail.com>
2013-04-08 02:20:23 +0800

    fix menu bar bug
    add mobile detection

--- CultCinema/web/js/base.js
+++ CultCinema/web/js/base.js
@@ -1,6 +1,6 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function(){
+    //Move menu bar with scroll of main content
+    $('#mainContainer').scroll(function(){
+        $('.scrollLeft').css('left', '-' + $('#mainContainer').scrollLeft() + 'px');
+    });
+})
