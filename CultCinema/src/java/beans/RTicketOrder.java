fb56ef65edf4135e577a8d2050b7337d0b0541be
duncan-wong <duncan1992@gmail.com>
2013-04-13 00:14:22 +0800

    Revert "Revert "merge""
    This reverts commit e98bf827a66e7d568ad38c3ffbad5361d054a83c.

---
+++ CultCinema/src/java/beans/RTicketOrder.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author DUNCAN
+ */
+public class RTicketOrder {
+    
+}
