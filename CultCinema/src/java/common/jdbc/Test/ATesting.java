edd51cf02b0a7132c33adbb5c454cc9ba067c4dd
duncan-wong <duncan1992@gmail.com>
2013-04-14 14:55:41 +0800

    bug fix: encode chinese character from request
    Convert specific parameter in request from ISO-8859-1(default encoding
    of HTTP POST) to UTF-8

--- CultCinema/src/java/common/jdbc/Test/ATesting.java
+++ CultCinema/src/java/common/jdbc/Test/ATesting.java
@@ -24,7 +24,8 @@ public class ATesting extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-
+        
+        request.setCharacterEncoding("UTF-8");
         String action = request.getParameter("action");
         if (action != null) {
             // call different action depends on the action parameter
@@ -43,6 +44,7 @@ public class ATesting extends HttpServlet {
         try {
             out.println("<html>");
             out.println("<head>");
+            out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
             out.println("<title>Phonebook Directory</title>");
             out.println("</head>");
             out.println("<body>");
@@ -50,7 +52,7 @@ public class ATesting extends HttpServlet {
             out.println("<div style='width:600px'>");
             out.println("<fieldset>");
             out.println("<legend>Search</legend>");
-            out.println("<form method='POST' action='" + request.getRequestURI() + "'>");
+            out.println("<form method='POST' action='" + request.getRequestURI() + "' enctype=\"application/x-www-form-urlencode\" accept-charset='UTF-8'>");
             out.println("<input name='action' type='hidden' value='search' />");
             out.println("<p>Name:");
             out.println("<input name='name' type='text' size='25' maxlength='255' value='' />");
@@ -66,10 +68,9 @@ public class ATesting extends HttpServlet {
     private void doSearchEntry(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            String searchName = request.getParameter("name");
+            String searchName = new String(request.getParameter("name").getBytes("ISO-8859-1"), "UTF-8");
 //-------------------------------------------------------------------------
             /**
             Context initCtx = new InitialContext();
@@ -89,12 +90,13 @@ public class ATesting extends HttpServlet {
             DBconnect db = new DBconnect();
             String procedure = "{ call show_movieDetail(?,default,default,?) }";
             db.prepareCall(procedure);
-            db.setXxx(1, "EN");
+            db.setXxx(1, "zh");
             db.setXxx(2, searchName);
             db.executeQuery();
 //-----------------------------------------------------------------------
             out.println("<html>");
             out.println("<head>");
+            out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
             out.println("<title>Phonebook Directory</title>");
             out.println("</head>");
             out.println("<body>");
